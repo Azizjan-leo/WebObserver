0b73065a90bd039350bae5e29eae7b127615d3e1
azizjan <azizjan.ayupov@gmail.com>
2022-10-08 13:10:48 +0600

    Add project files.

---
+++ WebObserver.Desktop/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace WebObserver.Desktop
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+        public ChromiumWebBrowser chromeBrowser;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
